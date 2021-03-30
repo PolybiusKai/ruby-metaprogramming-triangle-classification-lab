8a83bc2e2929b59c407a9c2590c7a60a6be9a0d6
rsmith <legendary.belief@gmail.com>
2021-03-30 16:20:26 -0700

    commit tree 7764d55a96cf2105acd265aa9e445d4eb2203930

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c 
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def validate
+    triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |side|
+      triangle << false if side <= 0 
+    raise TriangleError if triangle.include?(false)
+    }
+  end
+
+  def kind 
+    validate
+    if a == b && b == c 
+      :equilateral
+    elsif a == b || b == c || a == c 
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+    
+  class TriangleError < StandardError
+  end
 end
